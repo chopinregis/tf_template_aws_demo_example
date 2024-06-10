716e89fa6dd0d4050f8936f09423a7cf62713f6f
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-06-09 22:59:26 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,12 +1,15 @@
 # AWS Access Key ID
 variable "AWS_ACCESS_KEY" {
+  type = string
   # No default value provided; must be supplied by the user or through environment variables
 }
 
 # AWS Secret Access Key
 variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
   # No default value provided; must be supplied by the user or through environment variables
 }
+
 
 # AWS Region Configuration
 variable "aws_region" {
