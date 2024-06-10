10cf3e9452066abbd825f0ea6c480fe33bb4869d
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-06-09 22:59:07 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+# AWS Access Key ID
+variable "AWS_ACCESS_KEY" {
+  # No default value provided; must be supplied by the user or through environment variables
+}
+
+# AWS Secret Access Key
+variable "AWS_SECRET_ACCESS_KEY" {
+  # No default value provided; must be supplied by the user or through environment variables
+}
+
+# AWS Region Configuration
+variable "aws_region" {
+  description = "Region in which AWS resources are to be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+# EC2 Instance Type Configuration
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
