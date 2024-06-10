49279df268eec3fd2aacb93394554e8a8b25a4c3
Regis Nde Tene <chopinregis@users.noreply.github.com>
2024-06-09 22:48:19 -0400

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,9 +1,10 @@
-resource "aws_instance""myec2m"{
-    ami=data.aws_ami.amz_linux2.id
-    instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
-    count = 2
-    tags = {
-        name="count-demo-${count.index}"
-    }
+resource "aws_instance" "myec2m" {
+  ami                     = data.aws_ami.amz_linux2.id
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = [aws_security_group.vpc_ssh.id, aws_security_group.vpc_web.id]
+  count                   = 2
+
+  tags = {
+    Name = "count-demo-${count.index}"
+  }
 }
